ba248141470e5f069b90f0f72f31855648ca2649
José Duarte <duarte.gmj@gmail.com>
2023-06-07 17:18:04 +0100

    Initial Commit

---
+++ HckrNews/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  HckrNews
+//
+//  Created by José Duarte on 07/06/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HckrNews/HckrNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HckrNewsApp.swift
+//  HckrNews
+//
+//  Created by José Duarte on 07/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct HckrNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
