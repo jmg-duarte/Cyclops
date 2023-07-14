7e6d9dda533bb66837a2c79b599a16e291e752c6
José Duarte <duarte.gmj@gmail.com>
2023-07-14 18:40:03 +0100

    feat(#32): add hint on how to add bookmarks
    fixes #32

--- Cyclops/Views/BookmarksView.swift
+++ Cyclops/Views/BookmarksView.swift
@@ -10,25 +10,33 @@ struct BookmarksView: View {
 
     // I should probably add an "added" field to the bookmarks for sorting for now, fuck it
     @FetchRequest(sortDescriptors: [SortDescriptor(\.time, order: .reverse)]) var bookmarks: FetchedResults<Bookmark>
+    
     var body: some View {
         NavigationStack {
-            List {
-                ForEach(bookmarks) { bookmark in
-                    ItemView(
-                        id: Int(bookmark.id),
-                        url: bookmark.url!,
-                        title: bookmark.title!,
-                        time: Int(bookmark.time)
-                    )
+            if bookmarks.isEmpty {
+                Text("Swipe from left to right on an item to bookmark it!")
+                    .font(.caption)
+                    .foregroundColor(.gray)
+                    .navigationBarTitle(Text("Bookmarks"), displayMode: .inline)
+            } else {
+                List {
+                    ForEach(bookmarks) { bookmark in
+                        ItemView(
+                            id: Int(bookmark.id),
+                            url: bookmark.url!,
+                            title: bookmark.title!,
+                            time: Int(bookmark.time)
+                        )
+                    }
+                    // I wanted the slide to show a trash icon but this is the only way I could make it work
+                    // The .slideActions seems to delete the item, reload the view but not update the request
+                    // so when it takes out the bookmark again, it's been deleted and the unwrap fails
+                    // The explanation may be incorrect but for now, it's the best I know of
+                    .onDelete(perform: deleteBookmark)
                 }
-                // I wanted the slide to show a trash icon but this is the only way I could make it work
-                // The .slideActions seems to delete the item, reload the view but not update the request
-                // so when it takes out the bookmark again, it's been deleted and the unwrap fails
-                // The explanation may be incorrect but for now, it's the best I know of
-                .onDelete(perform: deleteBookmark)
+                .listStyle(.plain)
+                .navigationBarTitle(Text("Bookmarks"), displayMode: .inline)
             }
-            .navigationBarTitle(Text("Bookmarks"), displayMode: .inline)
-            .listStyle(.plain)
         }
     }
 
